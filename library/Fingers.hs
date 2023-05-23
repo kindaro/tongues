5414b5f09f4a36e86be400e6a013c82c23267588
Ignat Insarov <kindaro@gmail.com>
2023-05-23 11:32:28 +0300

    Arithmetic works.

--- library/Fingers.hs
+++ library/Fingers.hs
@@ -16,17 +16,16 @@ apply = (Abstraction ∘ Abstraction ∘ Abstraction) do
   Reference 2 `Application` Reference 0 `Application` (Reference 1 `Application` Reference 0)
 identity = apply `Application` true `Application` false
 
-inwrite ∷ ℕ → Saying → Saying → Saying
-inwrite target filling reference@(Reference this)
-  | this ≡ target = filling
-  | otherwise = reference
-inwrite target filling (Application function argument) = Application (inwrite target filling function) (inwrite target filling argument)
-inwrite target filling (Abstraction abstracted) = Abstraction (inwrite (target + 1) (bump filling) abstracted)
-
 overReferences ∷ (ℕ → ℕ → ℕ) → ℕ → Saying → Saying
 overReferences mapping depth (Reference this) = Reference (mapping depth this)
 overReferences mapping depth (Abstraction abstracted) = Abstraction (overReferences mapping (depth + 1) abstracted)
 overReferences mapping depth (Application function argument) = Application (overReferences mapping depth function) (overReferences mapping depth argument)
+
+face ∷ ℕ → ℕ → ℕ
+face threshold number | number ≥ threshold = number + 1 | otherwise = number
+
+degeneracy ∷ ℕ → ℕ → ℕ
+degeneracy threshold number | number > threshold = number − 1 | otherwise = number
 
 bump ∷ Saying → Saying
 bump = overReferences face 0
@@ -34,14 +33,12 @@ bump = overReferences face 0
 slump ∷ Saying → Saying
 slump = overReferences degeneracy 0
 
-face ∷ ℕ → ℕ → ℕ
-face threshold number | number ≥ threshold = number + 1 | otherwise = number
-
-(@) ∷ Saying → Saying → Saying
-function @ argument = Application function argument
-
-degeneracy ∷ ℕ → ℕ → ℕ
-degeneracy threshold number | number > threshold = number − 1 | otherwise = number
+inwrite ∷ ℕ → Saying → Saying → Saying
+inwrite target filling reference@(Reference this)
+  | this ≡ target = filling
+  | otherwise = reference
+inwrite target filling (Application function argument) = Application (inwrite target filling function) (inwrite target filling argument)
+inwrite target filling (Abstraction abstracted) = Abstraction (inwrite (target + 1) (bump filling) abstracted)
 
 ease ∷ Saying → Saying
 ease (Application (ease → Abstraction abstracted) argument) = slump (inwrite 0 (bump argument) abstracted)
@@ -57,3 +54,20 @@ sayingOfNatural ∷ ℕ → Saying = fmap (Abstraction ∘ Abstraction) do
   fix \ recurse → \ case
     Zero → Reference Zero
     Successor n → Application (Reference 1) (recurse n)
+
+add ∷ Saying
+add = (Abstraction ∘ Abstraction ∘ Abstraction) (compose `Application` Application (Reference 2) (Reference 0) `Application` Application (Reference 1) (Reference 0))
+
+compose ∷ Saying
+compose = (Abstraction ∘ Abstraction ∘ Abstraction) (Application (Reference 2) (Application (Reference 1) (Reference 0)))
+
+exponentiate ∷ Saying
+exponentiate = (Abstraction ∘ Abstraction) (Reference 1 `Application` (compose `Application` (Reference 0)) `Application` sayingOfNatural 1)
+
+(@) ∷ Saying → Saying → Saying
+function @ argument = Application function argument
+infixl 9 @
+
+(•) ∷ Saying → Saying → Saying
+after • before = compose @ after @ before
+infixl 8 •
--- test-suites/checks/Main.hs
+++ test-suites/checks/Main.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import Prelude.Fancy.QuickCheck
-import Prelude.Fancy hiding (i)
+import Prelude.Fancy hiding (exponent, i)
 
 import Niceties
 import Fingers
@@ -19,7 +19,18 @@ instance Arbitrary Fingers.Saying where
 
 main = checks do
   checkPropertiesOf "boolean numbers" do
-    checkProperty "true" \ left right → ease (true `Application` left `Application` right) ↔ left
-    checkProperty "false" \ left right → ease (false `Application` left `Application` right) ↔ right
+    checkProperty "true" \ left right → ease (true @ left @ right) ↔ left
+    checkProperty "false" \ left right → ease (false @ left @ right) ↔ right
   checkPropertiesOf "natural numbers" do
     checkProperty "zero = false" do sayingOfNatural Zero ↔ false
+    checkProperty "add = (+)" \ n m → fixedPoint ease (add @ sayingOfNatural n @ sayingOfNatural m) ↔ fixedPoint ease (sayingOfNatural (n + m))
+    checkProperty "compose = (×)" \ n m → fixedPoint ease (sayingOfNatural n • sayingOfNatural m) ↔ fixedPoint ease (sayingOfNatural (n × m))
+    checkProperty "exponentiate = (^)" \ n m → let
+      base = n `mod` 4
+      exponent = m `mod` 6
+      in fixedPoint ease (exponentiate  @ sayingOfNatural exponent @ sayingOfNatural base) ↔ fixedPoint ease (sayingOfNatural (base ^ exponent))
+  checkPropertiesOf "category" do
+    checkPropertiesOf "identity" do
+      checkProperty "before" \ f x → fixedPoint ease ((f • identity) @ x) ↔ fixedPoint ease (f @ x)
+      checkProperty "after" \ f x → fixedPoint ease ((identity • f) @ x) ↔ fixedPoint ease (f @ x)
+    checkProperty "associativity" \ f g h x → fixedPoint ease (((h • g) • f) @ x) ↔ fixedPoint ease ((h • (g • f)) @ x)
